5e9a097ef3e83de756fbc63d0892309b56594dce
VishruthBharadw <01ankith1998@gmail.com>
2020-06-29 14:12:24 +0530

    update

---
+++ src/app/service/login.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoginService {
+  ApiUrl = "http://localhost:4000";
+
+  constructor(private http: HttpClient) {}
+
+  generateOtp(data) {
+    return this.http.post(`${this.ApiUrl}/generate-otp`, data);
+  }
+
+  verifyOtp(data) {
+    return this.http.post(`${this.ApiUrl}/verify-otp`, data);
+  }
+}
